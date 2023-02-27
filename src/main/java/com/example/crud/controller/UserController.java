e0b53548c1ca6a801d96777baed59b8deaad6bcd
nguyen minh hoa <nguyenhoa28598@gmail.com>
2023-02-27 23:34:53 +0900

    thymeleaf について

--- src/main/java/com/example/crud/controller/UserController.java
+++ src/main/java/com/example/crud/controller/UserController.java
@@ -4,14 +4,21 @@ import com.example.crud.Service.UserService;
 import com.example.crud.model.dto.UserDto;
 import com.example.crud.request.CreateUserReq;
 import com.example.crud.request.UpdateUserReq;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -22,34 +29,79 @@ public class UserController {
 
 
     @GetMapping("/search")
-    public ResponseEntity<?> searchUser(@RequestParam(name = "keyword", required = false,defaultValue = "") String name){
+    public ResponseEntity<?> searchUser(@RequestParam(name = "keyword", required = false, defaultValue = "") String name) {
         List<UserDto> user = userService.searchUser(name);
 
         return ResponseEntity.ok(user);
     }
+
     @GetMapping("")
-    public ResponseEntity<?> getListUser(){
+    public ResponseEntity<?> getListUser() {
         List<UserDto> user = userService.getListUser();
         return ResponseEntity.ok(user);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable int id){
+    public ResponseEntity<?> getUserById(@PathVariable int id) {
         UserDto result = userService.getUserById(id);
         return ResponseEntity.ok(result);
     }
+
     @PostMapping("")
-    public ResponseEntity<?> creatUser(@RequestBody CreateUserReq req){
+    public ResponseEntity<?> creatUser(@RequestBody CreateUserReq req) {
         UserDto result = userService.createUser(req);
         return ResponseEntity.ok(result);
     }
+
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateUser(@Valid @RequestBody UpdateUserReq req, Errors errors, @PathVariable int id){
+    public ResponseEntity<?> updateUser(@Valid @RequestBody UpdateUserReq req, Errors errors, @PathVariable int id) {
         UserDto result = userService.updateUser(req, id);
         return ResponseEntity.ok(result);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable int id){
+    public ResponseEntity<?> deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
         return ResponseEntity.ok("Delete success");
     }
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @PostMapping("/profile")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws Exception {
+//        ImageUploadResponse response = imageDataService.uploadImage(file);
+        if (!file.isEmpty()) {
+            try {
+                String uploadsDir = "/uploads/";
+                String toUploads = request.getServletContext().getRealPath(uploadsDir);
+                if (!new File(toUploads).exists()) {
+                    new File(toUploads).mkdir();
+                }
+
+//                log.info("toUploads = {}", toUploads);
+                System.out.println(toUploads);
+
+                String orgName = file.getOriginalFilename();
+                String filePath = toUploads + orgName;
+                File dest = new File(filePath);
+                file.transferTo(dest);
+                System.out.println(filePath);
+                return ResponseEntity.ok("Upload success");
+            } finally {
+
+            }
+        }
+        return null;
+    }
+    @GetMapping("/main")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("date", new Date());
+        return "date";
+    }
+
 }
---
+++ src/main/java/com/example/crud/entity/ImageData.java
@@ -0,0 +1,44 @@
+package com.example.crud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//import javax.persistence.*;
+
+@Entity
+@Table(name = "imageData")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ImageData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    @Column(name = "imagedata", length = 1000)
+    private byte[] imageData;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
