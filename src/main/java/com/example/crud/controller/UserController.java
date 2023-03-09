60af6135cfba7765c564dfe43ec7e12fd62c00ee
nguyen minh hoa <nguyenhoa28598@gmail.com>
2023-03-09 14:08:54 +0900

    file image debug

--- src/main/java/com/example/crud/controller/UserController.java
+++ src/main/java/com/example/crud/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.crud.controller;
 
+import com.example.crud.Service.ImageDataService;
 import com.example.crud.Service.UserService;
+import com.example.crud.entity.Image;
 import com.example.crud.entity.Users;
+import com.example.crud.exception.InternalServerException;
+import com.example.crud.exception.NotFoundException;
 import com.example.crud.model.dto.UserDto;
 import com.example.crud.request.CreateUserReq;
 import com.example.crud.request.LoginReq;
@@ -15,11 +19,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 //@SessionAttributes("session")
@@ -73,43 +80,125 @@ public class UserController {
         return login(loginReq,model,session);
     }
 
+    private static String UPLOAD_DIR = System.getProperty("user.home") + "/media/upload";
+    @Autowired
+    private HttpServletRequest request;
+
     @PostMapping("/save")
     public String updateUser(@Valid @ModelAttribute UserDto req, @RequestParam("avatar_file") MultipartFile file) {
-        System.out.println(req.getName());
-        System.out.println(req.getBirthday());
-        UserDto result = new UserDto();
-        if (!file.isEmpty()) {
-            try {
-                String uploadsDir = "/uploads/";
-                String toUploads = request.getServletContext().getRealPath(uploadsDir);
-                if (!new File(toUploads).exists()) {
-                    new File(toUploads).mkdir();
-                }
 
-//                log.info("toUploads = {}", toUploads);
-                System.out.println(toUploads);
+            File uploadDir = new File(UPLOAD_DIR);
 
-                String orgName = file.getOriginalFilename();
-                String filePath = toUploads + orgName;
-                File dest = new File(filePath);
-                file.transferTo(dest);
-                result.setAvatar(filePath);
-                System.out.println(filePath);
-                System.out.println(file);
-                System.out.println(dest);
+            if (!uploadDir.exists()) {
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
+                uploadDir.mkdirs();
 
             }
-        }
-        req.setAvatar(result.getAvatar());
+            String originalFilename = file.getOriginalFilename();
 
-        userService.updateUser(req, req.getId());
+            String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);;
+
+            if (originalFilename != null && originalFilename.length() > 0) {
+
+                if (!extension.equals("png") && !extension.equals("jpg") && !extension.equals("gif") && !extension.equals("svg") && !extension.equals("jpeg")) {
+
+                    throw new NotFoundException("dgd");
+
+                }
+
+                try {
+
+                    Image img = new Image();
+
+                    img.setName(file.getName());
 
 
-        return "redirect:/users/information";
+
+                    img.setType(extension);
+
+
+//                    img.setId(Long.valueOf(UUID.randomUUID().toString()));
+
+                    String link = "/media/static/" + "1" + "." + extension;
+
+                    img.setLink(link);
+
+                    // Create file
+
+                    File serverFile = new File(UPLOAD_DIR + "/" + "1" + "." + extension);
+
+                    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+
+                    stream.write(file.getBytes());
+
+                    stream.close();
+                    UserDto result = new UserDto();
+                    result.setAvatar(link);
+
+                    req.setAvatar(result.getAvatar());
+
+                    userService.updateUser(req, req.getId());
+
+//                    imageService.save(img);
+
+//                    return ResponseEntity.ok(link);
+
+                } catch (Exception e) {
+
+                    throw new InternalServerException("Lỗi khi upload file");
+
+                }
+
+            }
+
+            throw new NotFoundException("File không hợp lệ");
+
+
+//        System.out.println(req.getName());
+//        System.out.println(req.getBirthday());
+//        UserDto result = new UserDto();
+//        if (!file.isEmpty()) {
+//            try {
+////                String uploadsDir = "/uploads/";
+//                String toUploads = System.getProperty("user.home") + "/media/upload";
+////                        request.getServletContext().getRealPath(uploadsDir);
+//                if (!new File(toUploads).exists()) {
+//                    new File(toUploads).mkdirs();
+//                }
+//
+//                String originalFilename = file.getOriginalFilename();
+//
+//                String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);;
+//
+////                log.info("toUploads = {}", toUploads);
+//                System.out.println(toUploads);
+//
+//                String orgName = file.getOriginalFilename();
+//                String filePath = toUploads + orgName;
+//                String link = "/media/static/" + "1" + "." + extension;
+//                File dest = new File(filePath);
+//                File serverFile = new File(toUploads + "/" + "1"+ "." + extension);
+//                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+//                stream.write(file.getBytes());
+//                stream.close();
+//                file.transferTo(dest);
+//                result.setAvatar(link);
+//                System.out.println(filePath);
+//                System.out.println(link);
+//                System.out.println(serverFile);
+//
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            } finally {
+//
+//            }
+//        }
+//        result.setAvatar(link);
+//        req.setAvatar(result.getAvatar());
+//
+//        userService.updateUser(req, req.getId());
+
+
     }
 
     @DeleteMapping("/{id}")
@@ -118,8 +207,7 @@ public class UserController {
         return ResponseEntity.ok("Delete success");
     }
 
-    @Autowired
-    private HttpServletRequest request;
+
 
     @PostMapping("/profile")
     public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws Exception {
